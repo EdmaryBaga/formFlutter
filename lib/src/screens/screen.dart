df7b53fae929a70e3327182e415b7dfc55009f4d
Edith Bautista <ebautista@centraldealarmas.com.mx>
2020-07-01 19:00:42 -0500

    :star: Se setea el formulario con el boton Enviar, esto por medio de un FormState

--- lib/src/screens/screen.dart
+++ lib/src/screens/screen.dart
@@ -6,11 +6,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+//declaramos una variable global 
+final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20),
       child: Form(
+        key: formKey,
         child: Column(
           children: <Widget>[
             emailField(),
@@ -33,7 +38,8 @@ Widget emailField(){
 }
 
 Widget passwordField(){
-  return TextField(
+  //para que el currentState funcione se debe usar un TextForField no un TextField
+  return TextFormField(
     decoration: InputDecoration(
       labelText:'contraseña',
       hintText: 'contraseña',
@@ -47,6 +53,8 @@ Widget submitField(){
     margin: EdgeInsets.only(top:20.0),
     child:RaisedButton(
     onPressed: (){
+      // con la variable global hacemos que se limpie el fom
+      formKey.currentState.reset();
     },
     child: Text('Enviar'),
   ),
